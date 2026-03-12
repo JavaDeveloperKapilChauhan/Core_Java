399652d7a5ebbebfd015c6e9a637d28b74359d99
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-12 08:38:54 +0530

    adding while loop

---
+++ _13_while_loop/WhileLoop.java
@@ -0,0 +1,19 @@
+package _13_while_loop;
+
+public class WhileLoop {
+
+
+    public static void main(String [] args){
+
+
+        int i=1 ;
+
+
+        while(i<10){
+            System.out.println(i+ " Isskander Chauhan") ;
+            i++ ;
+    }
+
+    } 
+    
+}
