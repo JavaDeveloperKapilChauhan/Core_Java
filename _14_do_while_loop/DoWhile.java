df814560d64d699d79b1cec540f32bc11b1dd6a1
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-12 08:45:04 +0530

    Adding do while program

---
+++ _14_do_while_loop/DoWhile.java
@@ -0,0 +1,14 @@
+package _14_do_while_loop;
+
+public class DoWhile {
+    public static void main(String [] args){
+
+        int i = 12 ;
+
+        do{
+            System.out.println("Isskander Chauhan for value: "+ i) ;
+        }while(i>20)  ;
+
+    }
+    
+}
