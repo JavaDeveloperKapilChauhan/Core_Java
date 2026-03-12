d085f48681f845de6b5223dcdd72d33649d0eea8
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-12 08:49:15 +0530

    adding again for loop

---
+++ _15_for_loop/For_Loop.java
@@ -0,0 +1,13 @@
+package _15_for_loop;
+
+public class For_Loop {
+
+    public static void main(String [] args){
+
+
+        for(int i=20 ; i>0 ; i--){
+            System.out.println(i + " Isskander Chauhan") ;
+        }
+    }
+    
+}
