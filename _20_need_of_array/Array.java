27bf07118c098287376ec6e45602d28f8e53344a
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-17 15:29:14 +0530

    adding array program

---
+++ _20_need_of_array/Array.java
@@ -0,0 +1,12 @@
+package _20_need_of_array;
+
+public class Array {
+
+    public static void main(String [] args){
+
+        int arr[] = {1,2,3,4,5}  ;
+        System.out.println(arr[1]) ;
+
+    }
+    
+}
