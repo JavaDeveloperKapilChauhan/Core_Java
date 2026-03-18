08aa65a71a4952c486f460fdf6a5b365135dfb86
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-19 03:34:17 +0530

    adding string and stringBuffer program

---
+++ _25_String/String1.java
@@ -0,0 +1,16 @@
+package _25_String ;
+
+
+public class String1{
+    public static void main(String [] args){
+        System.out.println("Hammir Chauhan") ;
+
+        String name = new String("Isskander Chauhan") ;
+
+        System.out.println(name.concat(" is a Rajasthani"))  ;
+
+
+
+    }
+
+}
---
+++ _26_StringBuffer_StringBuilder/Study_buffers.java
@@ -0,0 +1,17 @@
+// package _26_StringBuffer_StringBuilder;
+
+public class Study_buffers{
+    public static void main(String[] args){
+        // System.out.println("String Buffer and String Builder " + ": Isskander Chauhan") ;
+
+        StringBuffer sb = new StringBuffer("Navin") ;
+        sb.append(" Reddy") ;
+        System.out.println(sb)  ;
+
+
+        // Diiference between string buffer and string builder
+        // StringBuffer is thread safe and StringBuilder is not thread safe
+
+    }
+    
+}
