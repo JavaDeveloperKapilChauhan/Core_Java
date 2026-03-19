9726bb2dc3a821917d1922e3f58e4c364d4e7b26
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-19 13:30:13 +0530

    adding inheritance program

---
+++ _37_Inheritance/Inheritance.java
@@ -0,0 +1,32 @@
+package _37_Inheritance;
+
+// class Calc{
+
+//     public int add(int n1 , int n2){
+//         return n1+n2 ;
+//     }
+
+//      public int sub(int n1 , int n2){
+//         return n1-n2 ;
+//     }
+
+// }
+
+public class Inheritance {
+
+    public static void main(String [] args){
+
+        AdvCalc obj = new AdvCalc() ;
+        int r1 = obj.add(4, 5)  ;
+        int r2 = obj.sub(7, 3) ;
+        int r3 = obj.multi(5, 3) ;
+        int r4 = obj.div(15 , 4) ;
+
+        System.out.println(r1 + " " + r2) ;
+        System.out.println(r3 + " " + r4) ;
+
+        // System.out.println("It is a Inheritance program") ;
+
+    }
+    
+}
