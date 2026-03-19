7ad2e4b5410b687b5bc468d21c0c24af64bd5f75
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-19 15:51:15 +0530

    adding a program why java does not support mutiple inheritance through classes

---
+++ _39_multiple_inheritance/Multiple_Inheritance.java
@@ -0,0 +1,22 @@
+package _39_multiple_inheritance;
+
+
+class A{
+
+}
+
+class B{
+
+}
+
+class C extends A,B{
+
+}
+
+public class Multiple_Inheritance {
+    public static void main(String [] args){
+        System.out.println("Java does not support mutiple inheritance through classes.")
+        System.out.println("Isskander Chauhan Chauhan") ;
+    }
+    
+}
