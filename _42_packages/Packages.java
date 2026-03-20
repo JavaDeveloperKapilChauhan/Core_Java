d7502b252fce8d563bc78bd7df2e8b954a2fdd66
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-20 11:38:20 +0530

    adding packages programs

---
+++ _42_packages/Packages.java
@@ -0,0 +1,14 @@
+package _42_packages;
+import _42_packages.inner_folder.InnerClass ;
+
+
+public class Packages  {
+    public static void main(String [] args){
+
+        InnerClass p = new InnerClass()  ;
+        System.out.println(p.a) ;
+
+
+    }
+    
+}
---
+++ _42_packages/inner_folder/InnerClass.java
@@ -0,0 +1,8 @@
+package _42_packages.inner_folder;
+
+public class InnerClass {
+
+    public int a= 12 ;
+
+    
+}
