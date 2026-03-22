e0a6bc4673a20a7ad100e1e698a03436b4ff3bf7
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-22 14:12:35 +0530

    adding Exception program

---
+++ _63_Exception/Exception.java
@@ -0,0 +1,28 @@
+package _63_Exception;
+
+public class Exception {
+    public static void main(String [] args){
+        System.out.println("Exception are Runtime errors") ;
+
+
+        
+        System.out.println("It is a Exception Program") ;
+
+        System.out.println("Types of errors\nThere are three types of errors") ;
+        System.out.println("1.Compile type error\n2.Runtime error\n3.Logical error") ;
+
+        System.out.println("Compile Error -- The error occur when our compiler refuse to compile our code") ;
+        System.out.println("Runtime Error -- The error at Runtime means after compilation of code") ;
+        System.out.println("Logical error -- The error occured to wrong logic") ;
+        int a = 2 ; 
+        int b = 1 ;
+
+        System.out.println("The sum be: " + a+b) ;
+
+
+
+
+
+    }
+    
+}
