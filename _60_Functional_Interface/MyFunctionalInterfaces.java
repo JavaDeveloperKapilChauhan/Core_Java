acafbb27052b87ba9c1a299ad0422c3a4ea5c9ea
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-22 08:13:03 +0530

    adding Functional Interface Program

---
+++ _60_Functional_Interface/MyFunctionalInterfaces.java
@@ -0,0 +1,35 @@
+package _60_Functional_Interface;
+
+
+@FunctionalInterface
+interface A{
+    void show()  ;
+   
+}
+
+// class B implements A{
+//     public void show(){
+//         System.out.println("In B show") ;
+//     }
+    
+
+// }
+
+public class MyFunctionalInterfaces {
+    public static void main(String [] args){
+        System.out.println("It is a Functional Interface program..") ;
+
+
+        A obj = new A(){
+            public void show(){
+                System.out.println("in show") ;
+            }
+        } ;
+        obj.show() ;
+
+
+
+    }
+    
+}
+
