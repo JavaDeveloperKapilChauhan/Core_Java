b95b7805a0d4c79073b6ff4a1344f6ae81d747cb
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-24 16:06:33 +0530

    adding Collection Api Intro

---
+++ _77_Collection_API/CollectionApi.java
@@ -0,0 +1,13 @@
+package _77_Collection_API;
+
+public class CollectionApi {
+    public static void main(String[] args) {
+        System.out.println("Collection Api program");
+
+        System.out.println("Coillection Api is a concept");
+        System.out.println("Collection is a interface");
+        System.out.println("Collections is a class");
+
+    }
+
+}
