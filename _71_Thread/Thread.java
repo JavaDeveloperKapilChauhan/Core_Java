894c97fcd6f49d397e6fffc7bdcb726f1aca3aeb
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-24 09:27:36 +0530

    adding Thread program

---
+++ _71_Thread/Thread.java
@@ -0,0 +1,10 @@
+package _71_Thread;
+
+public class Thread {
+    public static void main(String [] args){
+
+        System.out.println("Threads are the smallest unit of execution, ");
+        System.out.println("A process contain many threads") ;
+    }
+    
+}
