956b3417defec340b2241a22802f4948131ec9be
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-25 17:21:05 +0530

    adding Map , Filter , Reduce , Sort with Stream

---
+++ _85_Map_Filter_Reduce/Map_Filter_Reduce.java
@@ -0,0 +1,37 @@
+package _85_Map_Filter_Reduce;
+import java.util.List ;
+import java.util.Arrays ;
+import java.util.stream.Stream ;
+
+
+
+public class Map_Filter_Reduce {
+    public static void main(String [] args){
+
+        System.out.println("Map ,Filter ,Reduce program");
+
+        List<Integer> nums = Arrays.asList(4,5,7,3,2,6) ;
+        System.out.println("nums be: " + nums) ;
+
+        // int result = nums.stream().filter(n-> n%2==0).map(n-> n*2).reduce(0, (c,e)->c+e) ;
+        // System.out.println("Result be : " + result) ;
+
+        Stream<Integer> s1 = nums.stream().filter(n-> n%2==0).map(n-> n*2).sorted() ;
+
+        System.out.println("s1 be: " + s1) ;
+        s1.forEach(n-> System.out.print(n + " ")) ;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
