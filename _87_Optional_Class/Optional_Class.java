29f6d4d348bc88ec2253deebef03fe46967b2b9d
kapil-git-del <kapil.kumar@rtctek.com>
2026-03-26 10:14:53 +0530

    adding Optional Class Program

---
+++ _87_Optional_Class/Optional_Class.java
@@ -0,0 +1,23 @@
+package _87_Optional_Class;
+import java.util.List ;
+import java.util.Arrays ;
+import java.util.Optional ;
+
+
+public class Optional_Class {
+    public static void main(String [] args){
+        System.out.println("Optional Class Program") ;
+
+        List<String> list = Arrays.asList("Isskander Chauhan" , "Hammir Chauhan" , "Prithviraj Chauhan") ;
+
+        Optional<String> name = list.stream().filter(n-> n.contains("a1")).findFirst()  ;
+
+        System.out.println("list be: "+list) ;
+
+        System.out.println("Optional Class is used to prevent NullPointerException") ;
+
+        System.out.println("name be : " + name.orElse("Not found")) ;
+    }
+
+    
+}
